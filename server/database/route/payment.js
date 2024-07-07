af13af64f6ab686fe8d59406b11e1ae0f43ecbf4
KhaledNacef <khaledncf0@gmail.com>
2024-07-07 04:40:23 +0100

    pay

--- server/database/route/payment.js
+++ server/database/route/payment.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const payrouter = express.Router();
-const paymen=require('../contorler/payment.js')
+// const express = require('express');
+// const payrouter = express.Router();
+// const paymen=require('../contorler/payment.js')
 
 
-payrouter.post('/pay',paymen.payment);
+// payrouter.post('/pay',paymen.payment);
 
-module.exports = payrouter
+// module.exports = payrouter
