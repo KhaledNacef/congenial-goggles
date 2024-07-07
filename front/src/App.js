05ca7e2204ccd247d40780ebe53a429ef251332c
KhaledNacef <khaledncf0@gmail.com>
2024-07-07 05:58:09 +0100

    done

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Signup from './home/signup.jsx';
 import Login from './home/login.jsx';
 import User from './user/user.jsx';
@@ -7,22 +7,16 @@ import { AuthProvider } from './AuthContext';
 import ProtectedRoute from './ProtectedRoute';
 
 const App = () => {
-  
-
-  
-
   return (
     <AuthProvider>
       <Router>
         <Routes>
-          
-          <Route path='/Signup' element={<Signup />} />
-          <Route path='/' element={<Login />} />
-          
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<Login />} />
 
           {/* Use ProtectedRoute for routes that require authentication */}
           <Route element={<ProtectedRoute />}>
-            <Route path='/user' element={<User />} />
+            <Route path="/user" element={<User />} />
           </Route>
         </Routes>
       </Router>
--- front/src/ProtectedRoute.js
+++ front/src/ProtectedRoute.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { Route, Navigate } from 'react-router-dom';
 import { useAuth } from './AuthContext';
 
-const ProtectedRoute = ({ component: Component, ...rest }) => {
+const ProtectedRoute = ({ element: Element, ...rest }) => {
   const { isAuthenticated } = useAuth();
 
   return (
-    <Route {...rest} element={isAuthenticated ? <Component /> : <Navigate to="/" />} />
+    <Route
+      {...rest}
+      element={isAuthenticated ? <Element /> : <Navigate to="/" />}
+    />
   );
 };
 
