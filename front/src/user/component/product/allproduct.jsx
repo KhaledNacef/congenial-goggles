7d6b98ba0173ddef543f79787702c3bca1dbd2ba
KhaledNacef <khaledncf0@gmail.com>
2024-07-16 06:45:45 +0100

    done

--- front/src/user/component/product/allproduct.jsx
+++ front/src/user/component/product/allproduct.jsx
@@ -13,7 +13,6 @@ import { Typography, IconButton, Box } from '@mui/material';
 import UpdateIcon from '@mui/icons-material/Update';
 import SellIcon from '@mui/icons-material/Sell';
 import EditIcon from '@mui/icons-material/Edit';
-import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 
 import axios from 'axios';
